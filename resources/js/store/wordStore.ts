7c2e5bb9a1777c4b45d925d3c249f686a10cddd8
Tim <timmyway1990@gmail.com>
2024-09-26 19:02:12 +0300

    feat: Use Vueuse to implement localstorage logic, so we can store efficiently word store setting.

--- resources/js/store/wordStore.ts
+++ resources/js/store/wordStore.ts
@@ -1,12 +1,12 @@
 import { ListMode } from "@/types/words/word.types";
+import { useLocalStorage } from "@vueuse/core";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useWordStore = defineStore('word', () => {
-    const setting = ref<{
-        listMode: ListMode
-    }> ({
-        listMode: 'normal',
+    // Sync the setting object with localStorage using useLocalStorage
+    const setting = useLocalStorage('word-settings', {
+        listMode: 'normal', // Default value if nothing is in localStorage
     });
 
     return { setting }
